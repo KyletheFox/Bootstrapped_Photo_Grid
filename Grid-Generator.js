82460846ede36e82a6ab14bf43d27191b27ebd08
KyletheFox <ko163@me.com>
2016-08-23 18:57:15 -0400

    bootstrapfinished and working. Added personal_profile pics to display

--- Grid-Generator.js
+++ Grid-Generator.js
@@ -1,6 +1,7 @@
 function generateGrid() {
 
 	var setupJson;	// set up info on how to build grid
+	var colIndex;
 	
 	// Loads settings json from server
 	$.getJSON( "settings.json", function(data) {  
@@ -14,21 +15,22 @@ function generateGrid() {
 
 		// -------- Logic ----------------------
 
-	
+		// Determines what bootstrapGrid size (col-xs-*, col-lg-*) is active
+		// and returns the setupJson.cols index that should be used.
+		colIndex = bootstrapGridSize(setupJson);
+		
 		// Gets the size of blocks that need to be created
-		console.log($(document));
-		dimentions = ($(window).outerWidth() / setupJson.cols);		
+		dimentions = ($(window).outerWidth() / setupJson.cols[colIndex]);		
 
 		// Determines the number of rows needed to fill screen
 		rowsToPrint = Math.ceil($(window).outerHeight()/dimentions);
 
 		// Gets the number of pictures loaded in from settings.json
 		numberOfPics = setupJson.pictures.length;
-
 		// ---------------------------------------
 
 		// ----- Creating HTML ------
-		DOMinsert(setupJson, rowsToPrint, numberOfPics, dimentions);
+		DOMinsert(setupJson, rowsToPrint, numberOfPics, dimentions, colIndex);
 		// --------------------------
 
 		// -------- Start Animations ---------
@@ -39,10 +41,12 @@ function generateGrid() {
 }
 
 // Creates HTML to display the grid
-function DOMinsert(setupJson, rowsToPrint, numberOfPics, dimentions) {
+function DOMinsert(setupJson, rowsToPrint, numberOfPics, dimentions, colIndex) {
 	
 	var returnStr = "";		// The string to attach
 	var num = 0;
+
+	console.log(colIndex);
 
 	$("body").append('<div class="container-fluid">');
 
@@ -51,7 +55,7 @@ function DOMinsert(setupJson, rowsToPrint, numberOfPics, dimentions) {
 		returnStr += '<div class="row">';
 		returnStr += '<div class="block-row col-xs-12 col-sm-12 col-md-12 col-lg-12">';
 
-		for (var i = 0; i < setupJson.cols; i++) {
+		for (var i = 0; i < setupJson.cols[colIndex]; i++) {
 			// Random index picture for background
 			var randomNum = Math.floor(Math.random() * numberOfPics);			
 			returnStr += '<div class="block pic-' +  num + '" id="row-' + j + '-col-' + i + '"class="block" style="height: ' + dimentions + 'px; width: ' + dimentions + 'px; background-image: url(' + setupJson.pictures[randomNum] +');"></div>';
@@ -65,17 +69,18 @@ function DOMinsert(setupJson, rowsToPrint, numberOfPics, dimentions) {
 
 	$(".container-fluid").append(returnStr);
 	$("body").append('</div>');
-	console.log(rowsToPrint * setupJson.cols);
-	startAnimation(setupJson, rowsToPrint, numberOfPics);
+	
+	startAnimation(setupJson, rowsToPrint, numberOfPics, colIndex);
 }
 
-function startAnimation(setupJson, rowsToPrint, numberOfPics) {
+// Starts the picture animations
+function startAnimation(setupJson, rowsToPrint, numberOfPics, colIndex) {
 		var blockID;	// Unique Block Indentifier
 		var time;		// Intervals for animation
 
-		console.log(rowsToPrint);
+		console.log(setupJson.cols[colIndex]);
 
-		for (var i = 0; i < setupJson.cols * rowsToPrint; i++) {
+		for (var i = 0; i < setupJson.cols[colIndex] * rowsToPrint; i++) {
 			time = (Math.random() * 15000) + 3000;
 			blockID = '.pic-' + i;
 			//console.log($(blockID));
@@ -87,14 +92,31 @@ function animation(time, id, setupJson) {
 
 	setInterval(function() {
 		var imgIndex = Math.floor(Math.random()* setupJson.pictures.length);
-		$(id).fadeOut(2000, function() {
+		$(id).fadeOut(setupJson.fadeOutSpeed, function() {
 			$(id).css("background-image", 'url("' + setupJson.pictures[imgIndex] + '")');
-			$(id).fadeIn(2000);
+			$(id).fadeIn(setupJson.fadeInSpeed);
 			//clearInterval(this);
 		});
 	}, time);
 }
 
-function callBlock() {
-	console.log($(".block"));
+function bootstrapGridSize(setupJson) {
+	// Returns index to setupJson.col after determining where col-xs, 
+	// col-sm-*, etc. is active.
+
+	var width = $(window).outerWidth();
+	var index;
+
+	if (width < 768) {
+		index = 0;
+	} else if (width >= 768 && width < 992) {
+		index = 1;
+	} else if (width >= 992 && width < 1200) {
+		index = 2;
+	} else {	// width >= 1200
+		index = 3;
+	}
+	console.log(width);
+	console.log(index);
+	return index;
 }
